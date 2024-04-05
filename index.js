3f66a4007048c228dceade33204454850db14b76
samedfft2634 <samedfft@gmail.com>
2024-04-05 23:43:45 +0300

    feat:Cookies-signUp-request

--- index.js
+++ index.js
@@ -3,13 +3,16 @@
 /*              IS TAKIP UYGULAMASI EXPRESSJS             */
 /* ====================================================== */
 const express = require("express");
-const {mongoose} = require("./models/user");
+const { mongoose } = require("./models/user");
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
+
+const cookieParser = require('cookie-parser')
 
 app.use(express.static("public"));
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser)
 
 app.set("view engine", "ejs");
 
@@ -19,7 +22,9 @@ mongoose
 	.connect(DB_URI)
 	.then((result) =>
 		app.listen(3000, () => {
-			console.log("*DB bağlantısı başarılı, server dinleniyor. http://localhost:3000 *");
+			console.log(
+				"*DB bağlantısı başarılı, server dinleniyor. http://localhost:3000 *"
+			);
 		})
 	)
 	.catch((err) => console.log(err));
@@ -29,4 +34,15 @@ app.get("/works", (req, res) => res.render("works"));
 
 /* =================================================== */
 // Routes
-app.use(require('./routes/authRouter'))
+app.use(require("./routes/authRouter"));
+
+app.get('/set-cookies',(req,res)=>{
+	res.cookie('yeni','false')
+	res.cookie('parola','12345',{maxAge:1000*60*60*24,httpOnly:true})
+	res.status(200).send('Cookie olustu.')
+})
+app.get('/get-cookies',(req,res)=>{
+const cookies = req.cookies;
+console.log(cookies)
+res.send(cookies)
+})
