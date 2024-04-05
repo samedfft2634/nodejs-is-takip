6c5b00bd970233f2ad7e67fb698c9fc431af830a
samedfft2634 <samedfft@gmail.com>
2024-04-05 22:59:15 +0300

    feat:signup-login-ejs

--- models/user.js
+++ models/user.js
@@ -1,37 +1,46 @@
-'use strict'
+"use strict";
 const mongoose = require("mongoose");
-const {isEmail} = require('validator')
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:[true,`Mail adresini girilmek zorundadir!\n`],
-        unique:true,
-        lowercase:true,
-        validate:[isEmail,`Lütfen geeçerli bir email adresi giriniz!\n`]
-    },
-    password:{
-        type:String,
-        required:[true,`Parola girilmek zorundadır!\n`],
-        minlength:[6,`Parola en az 6 karakter uzunluğunda olmak zorundadir!\n`]
-        
-    },
-},{
-    collection:"users",
-    timestamps:true
-})
+const userSchema = new mongoose.Schema(
+	{
+		email: {
+			type: String,
+			required: [true, `Mail adresini girilmek zorundadir!\n`],
+			unique: true,
+			lowercase: true,
+			validate: [isEmail, `Lütfen geeçerli bir email adresi giriniz!\n`],
+		},
+		password: {
+			type: String,
+			required: [true, `Parola girilmek zorundadır!\n`],
+			minlength: [
+				6,
+				`Parola en az 6 karakter uzunluğunda olmak zorundadir!\n`,
+			],
+		},
+	},
+	{
+		collection: "users",
+		timestamps: true,
+	}
+);
 
-userSchema.post('save',function(doc,next){
-    console.log('kaydediltikten sonra calisacak',doc)
-    next()
-})
+userSchema.post("save", function (doc, next) {
+	console.log("kaydediltikten sonra calisacak", doc);
+	next();
+});
 
-userSchema.pre('save',function(next){
-    console.log("kaydedilmeden once calisacak!!!",this)
-    next()
-})
+userSchema.pre("save", async function (next) {
+	// console.log("kaydedilmeden once calisacak!!!",this)
+	const salt = await bcrypt.genSalt();
+	this.password = await bcrypt.hash(this.password, salt);
 
-module.exports={
-    mongoose,
-    User: mongoose.model('User',userSchema)
-}
+	next();
+});
+
+module.exports = {
+	mongoose,
+	User: mongoose.model("User", userSchema),
+};
