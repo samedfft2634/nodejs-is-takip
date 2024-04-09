944c77ae5f3e1294849823dea1f8f7959ae4822b
samedfft2634 <samedfft@gmail.com>
2024-04-09 23:53:06 +0300

    feat:work_controller-router

---
+++ controllers/workController.js
@@ -0,0 +1,6 @@
+'use strict'
+/* _________________ workController ________________ */
+
+module.exports.work_add_get = (req,res)=>{
+    res.render('work-add')
+}
---
+++ routes/workRouter.js
@@ -0,0 +1,10 @@
+"use strict";
+/* ___________________ workRouter __________________ */
+
+const router = require("express").Router();
+const workController = require("../controllers/workController");
+const authToken = require("../middlewares/authMiddleware");
+
+router.get("/work-add", authToken, workController.work_add_get);
+
+module.exports = router;
